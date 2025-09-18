763b69ff7195225f31c44e407bb471603c12c831
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-18 09:26:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Laba_7_Alecseev_ISP_232_ISRPO
+{
+    class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public bool IsAvailable;
+        public Book()
+        {
+            Title = "неизвестно";
+            Author = "неизвестно";
+            Year = 0;
+            IsAvailable = false;
+        }
+        public Book(string title, string author, int year, bool IsAvailable)
+        {
+            this.Title = title;
+            this.Author = author;
+            this.Year = year;
+            this.IsAvailable = IsAvailable;
+        }
+
+        public void PrintInfo()
+        {
+            string b = IsAvailable ? "Да" : "Нет";
+
+            Console.WriteLine($"Название: \"{Title}\", Автор: {Author}, Год: {Year}, Доступна: {b}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание 1
+            //Book book1 = new Book();
+            //book1.PrintInfo();
+            //Book book2 = new Book("Война и мир", "Толстой", 1869, true);
+            //book2.PrintInfo();
+        }
+    }
+}
